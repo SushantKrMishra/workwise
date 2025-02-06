8b1df88990672dfddecec587f030cbe863c1f9ae
Sushant Kr Mishra <76252181+SushantKrMishra@users.noreply.github.com>
2025-02-06 17:27:22 +0530

    Update api.ts

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://workwise-jel2.onrender.com/",
 });
 
 API.interceptors.request.use(
